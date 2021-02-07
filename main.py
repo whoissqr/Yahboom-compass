1166260dc1c71270557d5cbb09e61d78d7f98a26
sqr <kk.shichao@gmail.com>
2021-02-07 13:35:11 +0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from microbit import *
 
-degrees = input.compass_heading()
-if (degrees < 45):
-   basic.show_string("N")
-elif (degrees >=45) and (degrees<135):
-   basic.show_string("E")
-elif (degrees >=135) and (degrees<225):
-    basic.show_string("S")
-elif (degrees > 225):
-    basic.show_string("W")
-else:
-    basic.show_string("N")
+def on_forever():
+    degrees = input.compass_heading()
+    if (degrees < 45):
+        basic.show_string("N")
+    elif degrees<135:
+        basic.show_string("E")
+    elif degrees<225:
+        basic.show_string("S")
+    elif degrees > 225:
+        basic.show_string("W")
+    else:
+        basic.show_string("N")
+
+basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-let degrees = input.compassHeading()
-if (degrees < 45) {
-    basic.showString("N")
-} else if (degrees >= 45 && degrees < 135) {
-    basic.showString("E")
-} else if (degrees >= 135 && degrees < 225) {
-    basic.showString("S")
-} else if (degrees > 225) {
-    basic.showString("W")
-} else {
-    basic.showString("N")
-}
-
+basic.forever(function on_forever() {
+    let degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else if (degrees > 225) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+    
+})
